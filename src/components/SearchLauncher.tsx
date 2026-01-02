8d9faf59cc462a2de0c693270decfeb72993c2b3
Dmitry Mezhinsky <mezhinsky.dmitry@gmail.com>
2026-01-02 13:23:40 +0300

    правка: цвета главной панели

--- src/components/SearchLauncher.tsx
+++ src/components/SearchLauncher.tsx
@@ -5,6 +5,8 @@ import { Search as SearchIcon } from "lucide-react";
 
 import { Kbd } from "@/components/ui/kbd";
 import { SearchDialog } from "@/components/Search";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 export function SearchLauncher() {
   const [open, setOpen] = useState(false);
@@ -31,19 +33,18 @@ export function SearchLauncher() {
       open={open}
       onOpenChange={setOpen}
       trigger={
-        <button
+        <Button
+          variant="outline"
           type="button"
           onClick={() => setOpen(true)}
-          className="inline-flex items-center gap-3 rounded-md bg-gray-100 transition hover:bg-gray-200 px-4 py-2 text-xs font-semibold text-gray-500  outline-none focus:ring-gray-300 dark:bg-gray-100 dark:text-gray-900 dark:focus:ring-gray-700"
+          className="inline-flex items-center gap-3 rounded-md transition outline-none text-xs  font-semibold"
         >
-          <span className="flex items-center gap-2">
+          <span className="hidden lg:flex items-center gap-2">
             <SearchIcon className="h-4 w-4" />
-            Поиск статей
+            Поиск
           </span>
-          <Kbd className="md-1 bg-white/20 text-gray dark:bg-black/10 dark:text-gray-900">
-            ⌘K
-          </Kbd>
-        </button>
+          <Kbd className="">⌘K</Kbd>
+        </Button>
       }
     />
   );
